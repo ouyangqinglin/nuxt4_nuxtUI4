9145729ee825d270533e090ca0b896b206cb770c
ouyangqinglin <ouyangqinglinlove@gmail.com>
2025-12-02 19:15:22 +0800

    chore()

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     modules: ['@nuxt/ui'],
     runtimeConfig: {
         public: {
-            apiBase: process.env.NUXT_PUBLIC_API_BASE
+            apiBase: ''
         }
     },
   css: [
