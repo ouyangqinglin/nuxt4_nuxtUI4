c1d39e418641a114ffd8005091904ab9b1b1739f
ouyangqinglin <ouyangqinglinlove@gmail.com>
2025-12-02 12:31:46 +0800

    chore()

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,34 +4,36 @@
     <h1 class="mt-40">Growth,<br />
       Driven by Technology.</h1>
     <h2 class="mt-8">Henrijayer is a world-leading expert in mobile <br />
-      marketingtechnology.</h2>
+      marketing technology.</h2>
     <h2 class="mt-8">
       We believe that in the complex ios ecosystem, the key to<br />
       unlocking sustainable growth for advertisers is the perfect<br />
-      synergy of intelligent technology and human expertise.
+    synergy of intelligent technology and human expertise.
     </h2>
-    <UForm :state="state" :validate="validate" @submit="onSubmit" @error="onError">
-      <UFormField label="Name" name="name">
-        <UInput v-model.trim="state.name" />
-      </UFormField>
-      <UFormField label="Company" name="company">
-        <UInput v-model.trim="state.company" />
-      </UFormField>
-      <UFormField label="Email" name="email">
-        <UInput v-model.trim="state.email" />
-      </UFormField>
-      <UFormField label="Business Needs" name="businessNeeds">
-        <UTextarea v-model.trim="state.businessNeeds" />
-      </UFormField>
-      <UButton type="submit">
-        Submit
-      </UButton>
-    </UForm>
+    <div>
+      <UForm :state="state" :validate="validate" @submit="onSubmit" @error="onError">
+        <UFormField label="Name" name="name">
+          <UInput v-model.trim="state.name" />
+        </UFormField>
+        <UFormField label="Company" name="company">
+          <UInput v-model.trim="state.company" />
+        </UFormField>
+        <UFormField label="Email" name="email">
+          <UInput v-model.trim="state.email" />
+        </UFormField>
+        <UFormField label="Business Needs" name="businessNeeds">
+          <UTextarea v-model.trim="state.businessNeeds" />
+        </UFormField>
+        <UButton type="submit">
+          Submit
+        </UButton>
+      </UForm>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { FormError, FormErrorEvent, FormSubmitEvent } from '@nuxt/ui'
+import type {FormError, FormErrorEvent, FormSubmitEvent } from '@nuxt/ui'
 const state = reactive({
   name: undefined,
   company: undefined,
@@ -42,7 +44,7 @@ const state = reactive({
 type Schema = typeof state
 
 function validate(state: Partial<Schema>): FormError[] {
-  const errors = []
+  const errors= []
   if (!state.name) errors.push({ name: 'name', message: 'Required' })
   if (!state.company) errors.push({ name: 'company', message: 'Required' })
   if (!state.email) errors.push({ name: 'email', message: 'Required' })
@@ -52,16 +54,16 @@ function validate(state: Partial<Schema>): FormError[] {
 
 const toast = useToast()
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
-  console.log(event.data)
+  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success'})
+console.log(event.data)
 }
 
 async function onError(event: FormErrorEvent) {
   if (event?.errors?.[0]?.id) {
     const element = document.getElementById(event.errors[0].id)
     element?.focus()
-    element?.scrollIntoView({ behavior: 'smooth', block: 'center' })
-  }
+    element?.scrollIntoView({ behavior: 'smooth', block: 'center'})
+ }
 }
 </script>
 
