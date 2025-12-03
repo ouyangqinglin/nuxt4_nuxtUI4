fd0c7fd35a28b36008360772b8945a9ae5ab9e06
ouyangqinglin <ouyangqinglinlove@gmail.com>
2025-12-03 10:00:59 +0800

    chore()

--- server/api/contact.ts
+++ server/api/contact.ts
@@ -1,12 +1,14 @@
-// export default defineEventHandler(async (event) => {
-//     const body = await readBody(event) // 接收前端请求体
-//     // 转发到真实外部接口（由服务端发出，不会触发浏览器 CORS）
-//     const res = await $fetch('https://asa.hljyer.com/v1/common/contact-form', {
-//         method: 'POST',
-//         body,
-//         headers: {
-//             'content-type': event.node.req.headers['content-type'] || 'application/json'
-//         }
-//     })
-//     return res
-// })
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event) // 接收前端请求体
+    // 转发到真实外部接口（由服务端发出，不会触发浏览器 CORS）
+    const config = useRuntimeConfig()
+    console.log('config api', config)
+    const res = await $fetch(`${config.public.apiBase}/api/v1/common/contact-form`, {
+        method: 'POST',
+        body,
+        headers: {
+            'content-type': event.node.req.headers['content-type'] || 'application/json'
+        }
+    })
+    return res
+})
