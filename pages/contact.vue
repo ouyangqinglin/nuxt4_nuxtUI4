babce602a04798f5c58353f6376eff813a3adb4c
ouyangqinglin <ouyangqinglinlove@gmail.com>
2025-12-04 16:11:31 +0800

    chore()

--- pages/contact.vue
+++ pages/contact.vue
@@ -64,14 +64,20 @@ function validate(state: Partial<Schema>): FormError[] {
 const toast = useToast()
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   const url = '/api/v1/common/contact-form'
+  const nonce = Math.random() + ''
+  const key = 'v0Wxg11Jl6hH2qxLf8'
   const data = await $fetch(url, {
     baseURL: config.public.apiBase,
     method: 'POST',
     body: {
       data: event.data,
       timestamp: +(new Date().getTime() / 1000).toFixed(0),
-      nonce: "5bf61e5d142cb77a79c37634e0494306",
-      sign: md5(JSON.stringify(event.data) + (new Date().getTime() / 1000).toFixed(0) + '5bf61e5d142cb77a79c37634e0494306' + 'v0Wxg11Jl6hH2qxLf8') // 这个sign是用md5加密的，加密规则 md5(JSON.stringify(data) + timestamp + nonce + 'v0Wxg11Jl6hH2qxLf8')
+      nonce,
+      sign: md5(JSON.stringify(event.data) + (new Date().getTime() / 1000).toFixed(0) + nonce + key) // 这个sign是用md5加密的，加密规则 md5(JSON.stringify(data) + timestamp + nonce + 'v0Wxg11Jl6hH2qxLf8')
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'language': 'en-US'
     }
   })
   console.log('data', data)
